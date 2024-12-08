254139b8bdbd334e2f7440501a1e1c90c308d91b
Aiysha Noor <161074894+AiyshaNoor2806@users.noreply.github.com>
2024-12-09 01:38:51 +0500

    update

--- src/app/HomePage/page.tsx
+++ src/app/HomePage/page.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Image from 'next/image'
 import { FaCheck } from 'react-icons/fa';
 import FoodCategory from '@/app/foodcategery/page'
-import foodpic1 from "/public/foodpic1.jpeg"
-import foodpic2 from "/public/foodpic2.jpeg"
-import foodpic3 from "/public/foodpic1.jpeg"
+import foodpic1 from "/foodpic1.jpeg"
+import foodpic2 from "/foodpic2.jpeg"
+import foodpic3 from "/foodpic1.jpeg"
 
 function HomePage() {
   return (
